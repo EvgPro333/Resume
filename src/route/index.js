f81ce3ea317f9869fb84c47797b72956f03ed712
EvgPro333 <tatyaakaunt@gmail.com>
2023-09-03 14:02:00 +0300

    Home Work

--- src/route/index.js
+++ src/route/index.js
@@ -23,7 +23,6 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
-    // title: 'Dmytro Ivanov | Resume',
     title: 'resume',
     name: {
       firstname: 'Dmytro',
@@ -43,6 +42,19 @@ router.get('/summary', function (req, res) {
         text: 'LinkedIn',
         href: 'https://www.linkedin.com/in/dmytro-test',
       },
+    },
+    salary: '600$ per month',
+    summary: {
+      title: 'Summary',
+      text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+              work on a new project I learn the domain and try to understand the idea of the project. Good team
+              player, every colleague is a friend to me.`,
+    },
+    experience: {
+      title: 'Other experience',
+      text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+				tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+				for such events like: money line - first win / draw / second win, totals etc.`,
     },
   })
 })
