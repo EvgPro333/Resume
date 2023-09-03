2c8b9fe7381989703f654a2b589db1ecf2eb7d53
EvgPro333 <tatyaakaunt@gmail.com>
2023-09-03 19:09:47 +0300

    lesson2

--- src/route/index.js
+++ src/route/index.js
@@ -18,44 +18,113 @@ router.get('/', function (req, res) {
 
 // ================================================================
 
-//              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/summary', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
-    // ↙ сюди вводимо JSON дані
-    title: 'resume',
-    name: {
-      firstname: 'Dmytro',
-      lastname: 'Ivanov',
+    page: {
+      title: 'resume',
     },
-    position: 'Junior Fullstack JS Developer',
-    social: {
-      email: {
-        text: 'dmytro@mail.com',
-        href: 'mailto:dmytro@mail.com',
+    // !                          end page   ----------------------
+
+    header: {
+      name: {
+        firstname: 'Dmytro',
+        lastname: 'Ivanov',
       },
-      phone: {
-        text: '+380670000123',
-        href: 'tel:+380670000123',
-      },
-      LinkedIn: {
-        text: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/dmytro-test',
-      },
+      position: 'Junior Fullstack JS Developer',
+      salary: '600$ в місяць',
     },
-    salary: '600$ per month',
-    summary: {
-      title: 'Summary',
-      text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+    // !                          end header   ----------------------
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
               work on a new project I learn the domain and try to understand the idea of the project. Good team
               player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+							tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+							for such events like: money line - first win / draw / second win, totals etc.`,
+      },
     },
-    experience: {
-      title: 'Other experience',
-      text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
-				tournament position, goals etc), analyzing by simple mathematics models and preparing probability
-				for such events like: money line - first win / draw / second win, totals etc.`,
+    // !                          end main   ----------------------
+
+    footer: {
+      social: {
+        email: {
+          text: 'dmytro@mail.com',
+          href: 'mailto:dmytro@mail.com',
+        },
+        phone: {
+          text: '+380670000123',
+          href: 'tel:+380670000123',
+        },
+        LinkedIn: {
+          text: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
     },
+    // !                          end footer   ----------------------
+  })
+})
+
+// ================================================================
+
+router.get('/skills', function (req, res) {
+  res.render('skills', {
+    page: {
+      title: 'resume',
+    },
+    // !                          end page   ----------------------
+
+    header: {
+      name: {
+        firstname: 'Dmytro',
+        lastname: 'Ivanov',
+      },
+      position: 'Junior Fullstack JS Developer',
+      salary: '600$ в місяць',
+    },
+    // !                          end header   ----------------------
+
+    main: {
+      skills: [
+        'HTML',
+        'HaldleBars',
+        'VS Code',
+        'Git',
+        'Terminal',
+        'NPM',
+      ],
+    },
+    // !                          end main   ----------------------
+
+    footer: {
+      social: {
+        email: {
+          text: 'dmytro@mail.com',
+          href: 'mailto:dmytro@mail.com',
+        },
+        phone: {
+          text: '+380670000123',
+          href: 'tel:+380670000123',
+        },
+        LinkedIn: {
+          text: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
+      //   // ???????? copyright типу так повинно було бути - не виходить!!!
+      //   headers: {
+      //     name: {
+      //       firstname: 'Dmytroooooo',
+      //     },
+      //   },
+    },
+    // !                          end footer   ----------------------
   })
 })
 
